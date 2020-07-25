d8a31b8c8299841f8d99a82a72deb3e71113bed0
Jay Patel <jaypatel32157@gmail.com>
2020-07-25 16:18:58 +0530

    New Navbar

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
 	  <?php
-		$home_class = $student_class = $faculty_class = $contact_class = "";
+		$home_class = $student_class = $faculty_class = $contact_class = $user_class= "";
 		$basename = basename($_SERVER['SCRIPT_NAME']);
 		if($basename == "dashboard.php"){
 			$home_class = 'class="active"';
@@ -20,12 +20,15 @@
 			$faculty_class = 'class="active"';
 		}else if($basename == "contact.php"){
 			$contact_class = 'class="active"';
+		}else if($basename == "user.php"){
+				$user_class = 'class="active"';
 		}
 	  ?>	
       <ul class="nav navbar-nav">
         <li <?php echo $home_class; ?> ><a href="dashboard.php">Home</a></li>
         <li <?php echo $student_class; ?>><a href="student.php">Students</a></li>
         <li <?php echo $faculty_class; ?>><a href="faculty.php">Faculty</a></li>
+		<li <?php echo $user_class; ?>><a href="user.php">User</a></li>
         <li <?php echo $contact_class; ?>><a href="#">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
