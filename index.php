bdc063be74e33e51e42c6828d522a64c6675ba29
Kishan-dhankecha <dhankechakishan@gmail.com>
2020-07-26 15:45:53 +0530

    small edit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<?php require("header.php"); ?>
+	<?php require("admin_header.php"); ?>
 
 
 </head>
